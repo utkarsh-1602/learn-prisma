c94c4c052e1172a12db647df9c650e85ea5616f3
utkarsh-1602 <usking619@gmail.com>
2023-11-03 14:27:38 +0530

    enhancement: client create operations tested

--- script.ts
+++ script.ts
@@ -1,8 +1,28 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({ log: ["query"] })
 
 async function main() {
-    const user = await prisma.user.deleteMany()
+    await prisma.user.deleteMany()
+    const user = await prisma.user.create({
+        data: {
+            name: "Utkarsh",
+            age: 21,
+            email: "utkarshuh16@gmail.com",
+            userPreference: {
+                create: {
+                    emailUpdates: true,
+                }
+            }
+        },
+        include: {
+            userPreference: true
+        }
+        // select: {
+        //     name: true,
+        // }
+        // we cannot do select and include, both at the same time
+    })
+
     console.log(user)
 }
 
