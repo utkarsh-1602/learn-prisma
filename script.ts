4607c8313bac27a9682623f1e04935a4977640d5
utkarsh-1602 <usking619@gmail.com>
2023-11-03 14:57:34 +0530

    enhancement: find unique constraints

--- script.ts
+++ script.ts
@@ -1,29 +1,51 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient({ log: ["query"] })
+const prisma = new PrismaClient()
 
 async function main() {
     await prisma.user.deleteMany()
-    const user = await prisma.user.create({
-        data: {
+    const users = await prisma.user.createMany({
+        data: [{
             name: "Utkarsh",
             age: 21,
             email: "utkarshuh16@gmail.com",
-            userPreference: {
-                create: {
-                    emailUpdates: true,
-                }
-            }
         },
-        include: {
-            userPreference: true
+        {
+            name: "naruto",
+            age: 20,
+            email: "iwillbehokage@gmail.com",
         }
-        // select: {
-        //     name: true,
-        // }
-        // we cannot do select and include, both at the same time
+        ],
     })
 
-    console.log(user)
+    // find the field by a unique constraint
+    const findField = await prisma.user.findUnique({
+        where: {
+            email: "utkarshuh16@gmail.com"
+        }
+    })
+
+    // Find the field by multiple unique constraints 
+    // in the schema.prisma we've defined a unique constraint for multiple fields [age, name]
+    const findMultipleFields = await prisma.user.findUnique({
+        where: {
+            age_name: {
+                age: 21,
+                name: "Utkarsh"
+            }
+        }
+    })
+
+
+    const findAnyField = await prisma.user.findFirst({
+        where: {
+            name: "Utkarsh"
+        }
+    })
+
+    console.log(users)
+    console.log(findField)
+    console.log(findMultipleFields)
+    console.log(findAnyField)
 }
 
 main()
