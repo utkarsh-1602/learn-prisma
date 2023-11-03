d49d155e21035ee6a506a7e5df24f30d98b7a719
utkarsh-1602 <usking619@gmail.com>
2023-11-03 15:58:27 +0530

    enhancement: advance filtering

--- script.ts
+++ script.ts
@@ -23,29 +23,87 @@ async function main() {
             name: "naruto",
             age: 20,
             email: "iwillbehokage@gmail.com",
-        }
+        },
+        {
+            name: "sasuke",
+            age: 20,
+            email: "uchiha@rediffmail.com",
+        },
+        {
+            name: "goku",
+            age: 50,
+            email: "ultrainstinct@saiyan.com",
+        },
         ],
     })
 
+
+    // Advance filtering
     const findManyFields = await prisma.user.findMany({
         where: {
-            name: "Utkarsh",
+            // name: { equals: "Utkarsh" },
+            name: { in: ["Utkarsh", "naruto"] }, // return all the users of given names            
         },
-        // distinct: ["name"],
-        // here you are getting first 2 users
-        take: 2,
-        // if you want to skip the 1st user, and return next 2 users, use skip
-        skip: 1,
-        // user orderby to get users by defined fields
-        orderBy: {
-            age: "desc" // descending
-        }
 
+    })
+
+    const findManyFields2 = await prisma.user.findMany({
+        where: {
+            // name: { equals: "Utkarsh" },
+            name: { notIn: ["Utkarsh"] }, // don't return the users of given names
+        },
+
+    })
+
+
+    const findManyFields3 = await prisma.user.findMany({
+        where: {
+            age: { lt: 30 } // age less than 20
+        },
+
+    })
+
+    const containsTheText = await prisma.user.findMany({
+        where: {
+            email: { contains: "@rediffmail.com" }, //checks if it contains the desired text in the given field
+            // you can also find the field values with "endsWith" or "startsWith"
+
+        }
+    })
+
+    //combining queries with AND operator
+    const combineQueries = await prisma.user.findMany({
+        where: {
+            AND: [
+                { email: { startsWith: "u" } },
+                { name: "Utkarsh" }
+            ],
+            OR: [
+                { age: { gt: 20 } },
+                { name: "naruto" }
+            ]
+        }
+    })
+
+    const combineQueries2 = await prisma.user.findMany({
+        where: {
+            OR: [
+                { age: { gt: 20 } },
+                { name: "naruto" }
+            ]
+        }
     })
 
 
     console.log(users)
-    console.log(findManyFields)
+    console.log(findManyFields.length)
+    console.log("findManyFields ===> ", findManyFields)
+    console.log("findManyFields2 ===> ", findManyFields2)
+    console.log("findManyFields3 ===> ", findManyFields3)
+    console.log("Contains text ===> ", containsTheText)
+    console.log("Combined Queries ====> ", combineQueries)
+    console.log("Combined Queries2 ====> ", combineQueries2)
+
 
 }
 main()
